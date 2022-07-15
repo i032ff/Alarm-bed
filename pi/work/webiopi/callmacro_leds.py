ed4407634d9b8ac1b67f6fbff3db9f6ddbe6d739
shotaro0310 <77211996+shotaro0310@users.noreply.github.com>
2022-07-16 01:08:12 +0900

    Update callmacro_leds.py

--- pi/work/webiopi/callmacro_leds.py
+++ pi/work/webiopi/callmacro_leds.py
@@ -1,52 +1,54 @@
 import webiopi
-
+import datetime
+ 
 GPIO = webiopi.GPIO
-
-LED1PIN = 23
-LED2PIN = 24
-LED3PIN = 25
-
-g_led1active = 0
-g_led2active = 0
-g_led3active = 0
-g_speed = 5
-
+ 
+LIGHT = 4    # GPIO4
+HOUR_ON = datetime.time(8,0)     # 自動消灯時間 8:00
+HOUR_OFF = datetime.time(18,0)     # 自動消灯時間 8:00
+ 
 def setup():
-	GPIO.setFunction( LED1PIN, GPIO.OUT )
-	GPIO.setFunction( LED2PIN, GPIO.OUT )
-	GPIO.setFunction( LED3PIN, GPIO.OUT )
-
+    # GPIO4をOUTに設定
+    GPIO.setFunction(LIGHT, GPIO.OUT)
+    # 現在時刻を取得
+    now = datetime.time(datetime.datetime.now().hour, datetime.datetime.now().minute)
+ 
+    # 現在時刻と自動点灯・消灯時間の比較
+    if ((now >= HOUR_ON ) and (now <= HOUR_OFF)):
+        GPIO.digitalWrite(LIGHT, GPIO.HIGH)
+ 
 def loop():
-	# speed          |   1    10
-	# interval[ sec] |   0.5   0.01
-	interval = 0.5 + (g_speed - 1) * (0.01 - 0.5) / (10-1)
-
-	if g_led1active:
-		GPIO.digitalWrite( LED1PIN, True )
-	if g_led2active:
-		GPIO.digitalWrite( LED2PIN, True )
-	if g_led3active:
-		GPIO.digitalWrite( LED3PIN, True )
-
-	webiopi.sleep( interval )
-
-	GPIO.digitalWrite(LED1PIN, False )
-	GPIO.digitalWrite(LED2PIN, False )
-	GPIO.digitalWrite(LED3PIN, False )
-
-	webiopi.sleep( interval )
-
+    # 現在時刻を取得
+    now = datetime.time(datetime.datetime.now().hour, datetime.datetime.now().minute)
+ 
+    # 自動点灯
+    if ((now.hour == HOUR_ON.hour) and (now.minute == HOUR_ON.minute) and (now.second == 0)):
+        if (GPIO.digitalRead(LIGHT) == GPIO.LOW):
+            GPIO.digitalWrite(LIGHT, GPIO.HIGH)
+ 
+    # 自動消灯
+    if ((now.hour == HOUR_OFF.hour) and (now.minute == HOUR_OFF.minute) and (now.second == 0)):
+        if (GPIO.digitalRead(LIGHT) == GPIO.HIGH):
+            GPIO.digitalWrite(LIGHT, GPIO.LOW)
+ 
+    # 1秒間隔で繰り返し
+    webiopi.sleep(1)
+ 
+def destroy():
+    # 消灯
+    GPIO.digitalWrite(LIGHT, GPIO.LOW)
+ 
 @webiopi.macro
-def ChangeLedActive( led, active ):
-	global g_led1active, g_led2active, g_led3active
-	if 1 == int(led):
-		g_led1active = int(active)
-	if 2 == int(led):
-		g_led2active = int(active)
-	if 3 == int(led):
-		g_led3active = int(active)
-
+def getLightHours():
+    return "%s;%s" % (HOUR_ON.strftime("%H:%M"),HOUR_OFF.strftime("%H:%M"))
+ 
 @webiopi.macro
-def ChangeSpeed( speed ):
-	global g_speed
-	g_speed = int(speed)
+def setLightHours(on, off):
+    global HOUR_ON, HOUR_OFF
+    # 引数を分割
+    array_on  = on.split(":")
+    array_off = off.split(":")
+    # 値の設定
+    HOUR_ON  = datetime.time(int(array_on[0]),int(array_on[1]))
+    HOUR_OFF = datetime.time(int(array_off[0]),int(array_off[1]))
+    return getLightHours()
