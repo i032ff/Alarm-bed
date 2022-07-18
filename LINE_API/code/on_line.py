1a8f04db226b4b20032fb8051c8ab6477ab12ffd
i032ff <tabs7seki@gmail.com>
2022-07-18 19:05:33 +0900

    アラーム　自動停止

--- LINE_API/code/on_line.py
+++ LINE_API/code/on_line.py
@@ -17,6 +17,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage
 )
+import textwrap
 
 app = Flask(__name__)
 
@@ -36,6 +37,23 @@ handler = WebhookHandler(channel_secret)
 # スイッチON・OFF関数の登録
 SWITCH_PIN = 23
 
+def SwitchOn():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SWITCH_PIN, GPIO.OUT)
+    GPIO.output(SWITCH_PIN, GPIO.HIGH)
+
+    # 30秒後停止
+    time.sleep(30)
+    GPIO.output(SWITCH_PIN, GPIO.LOW)
+    # GPIO.cleanup()
+
+
+def SwitchoOff():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SWITCH_PIN, GPIO.OUT)
+    GPIO.output(SWITCH_PIN, GPIO.LOW)
+    # GPIO.cleanup()
+
 m = Manager()
 db = m.dict()
 
@@ -50,7 +68,8 @@ def watcher(d):
                     temp = db[key]
                     item = temp.pop(0)                          
                     db[key] = temp
-                    print('設定時間になりました!')
+                    print('設定時間になりました!\n作動中です')
+                    SwitchOn()
                     line_bot_api.push_message(key, TextSendMessage(text='設定時間になりました!'))
         except Exception as e:
             print(e)
@@ -59,19 +78,6 @@ def watcher(d):
 
 p = Process(name='p1', target=watcher, args=(db, ))
 p.start()
-
-def SwitchOn():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(SWITCH_PIN, GPIO.OUT)
-    GPIO.output(SWITCH_PIN, GPIO.HIGH)
-    # GPIO.cleanup()
-
-
-def SwitchoOff():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(SWITCH_PIN, GPIO.OUT)
-    GPIO.output(SWITCH_PIN, GPIO.LOW)
-    # GPIO.cleanup()
 
 
 @app.route("/callback", methods=['POST'])
@@ -90,96 +96,122 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-# def message_text(event):
-#     text = event.message.text
-
-#     # テキストの内容で条件分岐
-#     if text == '作動':
-#         # 作動
-#         SwitchOn()
-#         # 返事
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage('目覚まし作動')
-#         )
-#     elif text == '停止':
-#         # 停止
-#         SwitchoOff()
-#         # 返事
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage('目覚まし停止')
-#         )
-#     elif text == '設定':
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage('目覚まし停止')
-#         )
-
-#     else:
-#         # 木霊
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(
-#                 text="指定された文字列ではありません\n[" + event.message.text + "]")
-#         )
 
 def handle_text_message(event):
     text = event.message.text #message from user
     reply = ""
-    print(event)
-    print(event.message)
-    print(dir(event.source))
-    print(vars(event.source))
-    inp = text.split(" ")   
-    inp.insert(0,event.source.user_id)
-    print(inp)    
-    try:
-        time_set = None
-        in_length = len(inp)
-        if in_length == 2:
-            inp_time = inp[1].split(":")
-            try:
-                hour = int(inp_time[0])
-                minute = 0
-                if len(inp_time) == 2:         
-                    minute=int(inp_time[1])
-                time_set = dt.now().replace(hour=hour,minute=minute,second=0)
-                reply = f'time{hour},{minute}'
-            except Exception:
-                reply = "時間は00-23まで、分は00-59までしか設定できません"
-        elif in_length == 3:
-            inp_date = inp[1].split("-")
-            inp_time = inp[2].split(":")
-            try:
-                day = int(inp_date[0])
-                month = int(inp_date[1])
-                year = int(inp_date[2])
-                hour = int(inp_time[0])
-                minute = 0
-                if len(inp_time) == 2:         
-                    minute=int(inp_time[1])
-                time_set = dt(year=year,month=month,day=day,hour=hour,minute=minute)
-            except Exception:
-                reply = "時刻の形式は dd-mm-yyy で、時間は 00-23 まで、分は 00-59 までです"
-        else:
-            reply = "日付/時間の過ぎています"
-        
-        if time_set is not None:    
-            try:
-                db[inp[0]]
-            except:
-                db[inp[0]] = []
-            if (time_set - dt.now()).total_seconds() > 0:
-                db[inp[0]] = db[inp[0]] + [time_set]
-                print(time_set)
-                alerm_time = time_set.strftime('%Y/%m/%d %H:%M')
-                reply = f'{alerm_time} にアラームが設定されました'
+
+    # print(event)
+    # print(event.message)
+    # print(dir(event.source))
+    # print(vars(event.source))
+
+    # テキストの内容で条件分岐
+    if text == '作動':
+        # 作動
+        SwitchOn()
+        # 返事
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage('目覚まし作動')
+        )
+    elif text == '停止':
+        # 停止
+        SwitchoOff()
+        # 返事
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage('目覚まし停止')
+        )
+    elif text == '説明':
+        s = '''
+        LINEからベッドのクッションを操作できる
+        「Laspberrybed」です!
+
+        画面下のボタンから、「この説明の確認」、「クッションの作動」、「クッションの停止」
+        ができます。
+
+        時刻のみを設定する場合には、
+        0:00
+        の形式で
+        日付も入力するなら、
+        2022-1-1 0:00
+        の形式で入力してください。
+        '''
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(textwrap.dedent(s)[1:-1])
+        )
+
+    else:
+        inp = text.split(" ")   
+        inp.insert(0,event.source.user_id)
+        # print(inp)
+
+        try:
+            time_set = None
+            in_length = len(inp)
+
+            if in_length == 2:  # 0:00の形式の場合
+                inp_time = inp[1].split(":")
+                print(inp)
+                try:
+                    hour = int(inp_time[0])
+                    minute = 0
+                    if len(inp_time) == 2:         
+                        minute=int(inp_time[1])
+                    time_set = dt.now().replace(hour=hour,minute=minute,second=0)
+                    print(inp_time)
+                    print(f'time{hour},{minute}')
+                    reply = f'time{hour},{minute}'
+
+                except Exception:
+                    reply = event.message.text + "は正しい形式ではありません"
+
+            elif in_length == 3:    #2022-1-1 0:00の形式の場合
+                inp_date = inp[1].split("-")
+                inp_time = inp[2].split(":")
+
+                try:
+                    set_year = int(inp_date[0])
+                    set_month = int(inp_date[1])
+                    set_day = int(inp_date[2])
+
+                    set_hour = int(inp_time[0])
+                    set_minute = 0
+                    if len(inp_time) == 2:         
+                        set_minute=int(inp_time[1])
+                        
+                    time_set = dt(set_year, set_month, set_day, set_hour, set_minute)
+
+                except Exception:
+                    reply = event.message.text + "は正しい形式ではありません"
+
             else:
-                reply = "時間が経過したため、アラームが正常に設定されませんでした"
-    except ValueError:
-        reply = "時間は00-23まで、分は00-59までしか設定できません"
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply),timeout=10) #返信する
+                reply = "日付/時間が過ぎています"
+            
+            if time_set is not None:
+
+                try:
+                    db[inp[0]]
+
+                except:
+                    db[inp[0]] = []
+
+                if (time_set - dt.now()).total_seconds() > 0:
+                    db[inp[0]] = db[inp[0]] + [time_set]
+                    print(time_set)
+                    alerm_time = time_set.strftime('%Y/%m/%d %H:%M')
+                    reply = f'{alerm_time} にアラームが設定されました'
+
+                else:
+                    reply = "時間が経過したため、アラームが正常に設定されませんでした"
+
+        except ValueError:
+            # 木霊
+            reply = "予約された文字列ではありません\n[" + event.message.text + "]"
+
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply),timeout=10) #返信する
 
 
 if __name__ == "__main__":
